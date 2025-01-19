fe1b4c1101db3c16eaad9e3e4d5f7ec771a08e3f
Frédéric Joanis <frederic.joanis@gmail.com>
2025-01-19 09:16:10 -0500

    Add Webgpu support

--- crates/bits_helpers/src/emoji.rs
+++ crates/bits_helpers/src/emoji.rs
@@ -1,5 +1,3 @@
-use std::path::PathBuf;
-
 use bevy::prelude::*;
 use bevy::render::render_resource::TextureFormat;
 use bevy::utils::default;
@@ -36,15 +34,14 @@ impl Plugin for EmojiPlugin {
 const ATLAS_SIZE: UVec2 = UVec2::new(8192, 8192);
 const EMOJI_SIZE: UVec2 = UVec2::new(128, 128);
 
-fn get_atlas_path() -> String {
-    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.push("assets");
-    path.push("emojis");
-    path.push("EmojiAtlas.png");
-    path.to_str()
-        .expect("Asset path contains invalid UTF-8 characters")
-        .to_string()
-}
+#[cfg(not(target_arch = "wasm32"))]
+pub const ATLAS_PATH: &str = "../../bits_helpers/assets/emojis/EmojiAtlas.png";
+#[cfg(target_arch = "wasm32")]
+pub const ATLAS_PATH: &str = concat!(
+    "../../bits_helpers-",
+    env!("CARGO_PKG_VERSION"),
+    "/assets/emojis/EmojiAtlas.png"
+);
 
 #[derive(Error, Debug)]
 pub enum AtlasError {
@@ -83,7 +80,7 @@ fn setup_emoji_atlas(
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    let texture_handle = asset_server.load(get_atlas_path());
+    let texture_handle = asset_server.load(ATLAS_PATH);
     let cols = ATLAS_SIZE.x / EMOJI_SIZE.x;
     let rows = ATLAS_SIZE.y / EMOJI_SIZE.y;
     let layout = TextureAtlasLayout::from_grid(EMOJI_SIZE, cols, rows, None, None);
